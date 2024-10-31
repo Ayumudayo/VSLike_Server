83845bfa944c50e996b0ffc576a215e98f567afb
Ayumudayo <yumemiayumu@gmail.com>
2024-10-31 15:01:57 +0900

    Cleanup `utils` folder

--- src/utils/inform/game.inform.js
+++ src/utils/inform/game.inform.js
@@ -5,24 +5,24 @@ import { getProtoMessages } from '../../init/loadProtos.js';
 import { PACKET_TYPE } from '../../constants/header.js';
 
 const makeInformPacket = (message, type) => {
-  const packetLength = Buffer.alloc(config.packet.totalLength);
-  packetLength.writeUInt32BE(
-    message.length + config.packet.totalLength + config.packet.typeLength,
-    0,
-  );
+    const packetLength = Buffer.alloc(config.packet.totalLength);
+    packetLength.writeUInt32BE(
+        message.length + config.packet.totalLength + config.packet.typeLength,
+        0,
+    );
 
-  const packetType = Buffer.alloc(config.packet.typeLength);
-  packetType.writeUInt8(type, 0);
+    const packetType = Buffer.alloc(config.packet.typeLength);
+    packetType.writeUInt8(type, 0);
 
-  return Buffer.concat([packetLength, packetType, message]);
+    return Buffer.concat([packetLength, packetType, message]);
 };
 
 export const createLocationPacket = (users) => {
-  const protoMessage = getProtoMessages();
-  const Location = protoMessage.gameInform.LocationUpdate;
+    const protoMessage = getProtoMessages();
+    const Location = protoMessage.gameInform.LocationUpdate;
 
-  const payload = { users };
-  const message = Location.create(payload);
-  const locationPacket = Location.encode(message).finish();
-  return makeInformPacket(locationPacket, PACKET_TYPE.LOCATION);
+    const payload = { users };
+    const message = Location.create(payload);
+    const locationPacket = Location.encode(message).finish();
+    return makeInformPacket(locationPacket, PACKET_TYPE.LOCATION);
 };
--- src/utils/parser/packetParser.js
+++ src/utils/parser/packetParser.js
@@ -7,62 +7,62 @@ import CustomError from '../error/customError.js';
 import { ErrorCodes } from '../error/errorCodes.js';
 
 export const packetParser = (data) => {
-  const protoMessages = getProtoMessages();
-  // 공통 패킷 구조를 디코딩
-  const Packet = protoMessages.common.Packet;
-  let decodedPacket;
-  try {
-    decodedPacket = Packet.decode(data);
-  } catch (e) {
-    throw new CustomError(ErrorCodes.PACKET_DECODE_ERROR, '패킷 디코딩 중 오류가 발생했습니다.');
-  }
+    const protoMessages = getProtoMessages();
+    // 공통 패킷 구조를 디코딩
+    const Packet = protoMessages.common.Packet;
+    let decodedPacket;
+    try {
+        decodedPacket = Packet.decode(data);
+    } catch (e) {
+        throw new CustomError(ErrorCodes.PACKET_DECODE_ERROR, '패킷 디코딩 중 오류가 발생했습니다.');
+    }
+    
+    const handlerId = decodedPacket.handlerId;
+    const userId = decodedPacket.userId;
+    const clientVersion = decodedPacket.version;
+    const sequence = decodedPacket.sequence;
 
-  const handlerId = decodedPacket.handlerId;
-  const userId = decodedPacket.userId;
-  const clientVersion = decodedPacket.version;
-  const sequence = decodedPacket.sequence;
+    if (clientVersion !== config.client.version) {
+        throw new CustomError(
+            ErrorCodes.CLIENT_VERSION_MISMATCH,
+            '클라이언트 버전이 일치하지 않습니다.',
+        );
+    }
 
-  if (clientVersion !== config.client.version) {
-    throw new CustomError(
-      ErrorCodes.CLIENT_VERSION_MISMATCH,
-      '클라이언트 버전이 일치하지 않습니다.',
-    );
-  }
+    const protoTypeName = getProtoTypeNameByHandlerId(handlerId);
+    if (!protoTypeName) {
+        throw new CustomError(ErrorCodes.UNKNOWN_HANDLER_ID, `알 수 없는 핸들러 ID: ${handlerId}`);
+    }
 
-  const protoTypeName = getProtoTypeNameByHandlerId(handlerId);
-  if (!protoTypeName) {
-    throw new CustomError(ErrorCodes.UNKNOWN_HANDLER_ID, `알 수 없는 핸들러 ID: ${handlerId}`);
-  }
+    const [namespace, typeName] = protoTypeName.split('.');
+    const PayloadType = protoMessages[namespace][typeName];
+    let decodedPayload;
 
-  const [namespace, typeName] = protoTypeName.split('.');
-  const PayloadType = protoMessages[namespace][typeName];
-  let decodedPayload;
+    try {
+        decodedPayload = PayloadType.decode(decodedPacket.payload);
+    } catch (e) {
+        throw new CustomError(ErrorCodes.PACKET_DECODE_ERROR, '패킷 디코딩 중 오류가 발생했습니다.');
+    }
 
-  try {
-    decodedPayload = PayloadType.decode(decodedPacket.payload);
-  } catch (e) {
-    throw new CustomError(ErrorCodes.PACKET_DECODE_ERROR, '패킷 디코딩 중 오류가 발생했습니다.');
-  }
+    const errorMessage = PayloadType.verify(decodedPayload);
+    if (errorMessage) {
+        throw new CustomError(
+            ErrorCodes.INVALID_PACKET,
+            `패킷 구조가 일치하지 않습니다: ${errorMessage}`,
+        );
+    }
 
-  const errorMessage = PayloadType.verify(decodedPayload);
-  if (errorMessage) {
-    throw new CustomError(
-      ErrorCodes.INVALID_PACKET,
-      `패킷 구조가 일치하지 않습니다: ${errorMessage}`,
-    );
-  }
+    // 필드가 비어있는 경우 = 필수 필드가 누락된 경우
+    const expectedFields = Object.keys(PayloadType.fields);
+    const actualFields = Object.keys(decodedPayload);
+    const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
 
-  // 필드가 비어있는 경우 = 필수 필드가 누락된 경우
-  const expectedFields = Object.keys(PayloadType.fields);
-  const actualFields = Object.keys(decodedPayload);
-  const missingFields = expectedFields.filter((field) => !actualFields.includes(field));
+    if (missingFields.length > 0) {
+        throw new CustomError(
+            ErrorCodes.MISSING_FIELDS,
+            `필수 필드가 누락되었습니다: ${missingFields.join(', ')}`,
+        );
+    }
 
-  if (missingFields.length > 0) {
-    throw new CustomError(
-      ErrorCodes.MISSING_FIELDS,
-      `필수 필드가 누락되었습니다: ${missingFields.join(', ')}`,
-    );
-  }
-
-  return { handlerId, userId, payload: decodedPayload, sequence };
+    return { handlerId, userId, payload: decodedPayload, sequence };
 };
