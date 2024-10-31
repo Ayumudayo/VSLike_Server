965e7b211d55c976eeac5a12615ebe704ac67cd6
Ayumudayo <yumemiayumu@gmail.com>
2024-10-31 15:00:30 +0900

    Cleanup `session` folder

--- src/session/game.session.js
+++ src/session/game.session.js
@@ -4,18 +4,18 @@ import Game from '../classes/models/game.class.js';
 import { gameSessions } from './sessions.js';
 
 export const addGameSession = (id) => {
-  const game = new Game(id);
-  return gameSessions.add(game);
+    const game = new Game(id);
+    return gameSessions.add(game);
 };
 
 export const removeGameSession = () => {
-  delete gameSessions[0];
+    delete gameSessions[0];
 };
 
 export const getGameSession = () => {
-  return gameSessions.sessions[0];
+    return gameSessions.sessions[0];
 };
 
 export const getAllGameSessions = () => {
-  return gameSessions.getAll();
+    return gameSessions.getAll();
 };
--- src/session/user.session.js
+++ src/session/user.session.js
@@ -4,30 +4,30 @@ import { userSessions } from './sessions.js';
 import { updateUserLocation } from '../db/user/user.db.js';
 
 export const addUser = (user) => {
-  return userSessions.add(user);
+    return userSessions.add(user);
 };
 
 export const removeUser = async (socket) => {
-  const user = userSessions.remove((user) => user.socket === socket);
-  if (user) {
-    await updateUserLocation(user.x, user.y, user.id);
-  }
-  return user;
+    const user = userSessions.remove((user) => user.socket === socket);
+    if (user) {
+        await updateUserLocation(user.x, user.y, user.id);
+    }
+    return user;
 };
 
 export const getNextSequence = (id) => {
-  const user = getUserById(id);
-  return user ? user.getNextSequence() : null;
+    const user = getUserById(id);
+    return user ? user.getNextSequence() : null;
 };
 
 export const getUserById = (id) => {
-  return userSessions.find((user) => user.id === id);
+    return userSessions.find((user) => user.id === id);
 };
 
 export const getUserBySocket = (socket) => {
-  return userSessions.find((user) => user.socket === socket);
+    return userSessions.find((user) => user.socket === socket);
 };
 
 export const getAllUsers = () => {
-  return userSessions.getAll();
+    return userSessions.getAll();
 };
