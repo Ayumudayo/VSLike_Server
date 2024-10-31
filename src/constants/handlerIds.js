5f436cc0fb3dc36d338cf92f685cf84026f6c87c
Ayumudayo <yumemiayumu@gmail.com>
2024-10-31 15:00:50 +0900

    Cleanup `constants` folder

--- src/constants/handlerIds.js
+++ src/constants/handlerIds.js
@@ -1,7 +1,7 @@
 export const RESPONSE_SUCCESS_CODE = 0;
 
 export const HANDLER_IDS = {
-  INITIAL: 0,
-  PING: 1,
-  UPDATE_LOCATION: 11,
+    INITIAL: 0,
+    PING: 1,
+    UPDATE_LOCATION: 11,
 };
