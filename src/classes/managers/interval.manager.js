e0f9a0593868c560965b51a370eb7c53fee9f6a1
Ayumudayo <yumemiayumu@gmail.com>
2024-10-31 15:01:28 +0900

    Cleanup `classes` folder

--- src/classes/managers/interval.manager.js
+++ src/classes/managers/interval.manager.js
@@ -3,56 +3,56 @@
 import BaseManager from './base.manager.js';
 
 class IntervalManager extends BaseManager {
-  constructor() {
-    super();
-    this.playerIntervals = new Map();
-    this.globalIntervals = new Map();
-  }
-
-  // 플레이어별 인터벌 추가
-  addPlayerInterval(playerId, callback, interval, type = 'updatePosition') {
-    if (!this.playerIntervals.has(playerId)) {
-      this.playerIntervals.set(playerId, new Map());
+    constructor() {
+        super();
+        this.playerIntervals = new Map();
+        this.globalIntervals = new Map();
     }
-    const intervalId = setInterval(callback, interval);
-    this.playerIntervals.get(playerId).set(type, intervalId);
-  }
 
-  // 플레이어별 인터벌 제거
-  removePlayerIntervals(playerId) {
-    if (this.playerIntervals.has(playerId)) {
-      const intervals = this.playerIntervals.get(playerId);
-      intervals.forEach((intervalId) => clearInterval(intervalId));
-      this.playerIntervals.delete(playerId);
+    // 플레이어별 인터벌 추가
+    addPlayerInterval(playerId, callback, interval, type = 'updatePosition') {
+        if (!this.playerIntervals.has(playerId)) {
+            this.playerIntervals.set(playerId, new Map());
+        }
+        const intervalId = setInterval(callback, interval);
+        this.playerIntervals.get(playerId).set(type, intervalId);
     }
-  }
 
-  // 글로벌 인터벌 추가
-  addGlobalInterval(id, callback, interval) {
-    const intervalId = setInterval(callback, interval);
-    this.globalIntervals.set(id, intervalId);
-  }
-
-  // 글로벌 인터벌 제거
-  removeGlobalInterval(id) {
-    if (this.globalIntervals.has(id)) {
-      clearInterval(this.globalIntervals.get(id));
-      this.globalIntervals.delete(id);
+    // 플레이어별 인터벌 제거
+    removePlayerIntervals(playerId) {
+        if (this.playerIntervals.has(playerId)) {
+            const intervals = this.playerIntervals.get(playerId);
+            intervals.forEach((intervalId) => clearInterval(intervalId));
+            this.playerIntervals.delete(playerId);
+        }
     }
-  }
 
-  // 모든 인터벌 제거
-  clearAllIntervals() {
-    this.playerIntervals.forEach((intervals) => {
-      intervals.forEach((intervalId) => clearInterval(intervalId));
-    });
-    this.playerIntervals.clear();
+    // 글로벌 인터벌 추가
+    addGlobalInterval(id, callback, interval) {
+        const intervalId = setInterval(callback, interval);
+        this.globalIntervals.set(id, intervalId);
+    }
 
-    this.globalIntervals.forEach((intervalId) => {
-      clearInterval(intervalId);
-    });
-    this.globalIntervals.clear();
-  }
+    // 글로벌 인터벌 제거
+    removeGlobalInterval(id) {
+        if (this.globalIntervals.has(id)) {
+            clearInterval(this.globalIntervals.get(id));
+            this.globalIntervals.delete(id);
+        }
+    }
+
+    // 모든 인터벌 제거
+    clearAllIntervals() {
+        this.playerIntervals.forEach((intervals) => {
+            intervals.forEach((intervalId) => clearInterval(intervalId));
+        });
+        this.playerIntervals.clear();
+
+        this.globalIntervals.forEach((intervalId) => {
+            clearInterval(intervalId);
+        });
+        this.globalIntervals.clear();
+    }
 }
 
 export default IntervalManager;
--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -3,26 +3,26 @@
 import BaseEntity from './base.entity.js';
 
 class User extends BaseEntity {
-  constructor(socket, id, playerId, latency, coords = { x: 0, y: 0 }) {
-    super(id, 'user', coords);
-    this.socket = socket;
-    this.playerId = playerId;
-    this.latency = latency;
-    this.sequence = 1;
-  }
-  
-  setLatency(latency) {
-    this.latency = latency;
-  }
-  
-  getLatency() {
-    return this.latency;
-  }
+    constructor(socket, id, playerId, latency, coords = { x: 0, y: 0 }) {
+        super(id, 'user', coords);
+        this.socket = socket;
+        this.playerId = playerId;
+        this.latency = latency;
+        this.sequence = 1;
+    }
 
-  getNextSequence() {
-    this.sequence += 1;
-    return this.sequence;
-  }
+    setLatency(latency) {
+        this.latency = latency;
+    }
+
+    getLatency() {
+        return this.latency;
+    }
+
+    getNextSequence() {
+        this.sequence += 1;
+        return this.sequence;
+    }
 }
 
 export default User;
